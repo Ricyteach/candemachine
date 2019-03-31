961c57b118fdf5db4dbdf1d02b28d3f383009079
Ricky Teachey <ricky@teachey.org>
2019-03-31 02:24:39 +0000

    skeleton classes for candetypes

---
+++ candemachine/candetypes.py
@@ -0,0 +1,49 @@
+from .candemain import CandeBase
+
+
+class CandeL1AnalysisASD(CandeBase):
+    pass
+
+
+class CandeL2AnalysisASD(CandeBase):
+    pass
+
+
+class CandeL3AnalysisASD(CandeBase):
+    pass
+
+
+class CandeL1DesignASD(CandeBase):
+    pass
+
+
+class CandeL2DesignASD(CandeBase):
+    pass
+
+
+class CandeL3DesignASD(CandeBase):
+    pass
+
+
+class CandeL1AnalysisLRFD(CandeBase):
+    pass
+
+
+class CandeL2AnalysisLRFD(CandeBase):
+    pass
+
+
+class CandeL3AnalysisLRFD(CandeBase):
+    pass
+
+
+class CandeL1DesignLRFD(CandeBase):
+    pass
+
+
+class CandeL2DesignLRFD(CandeBase):
+    pass
+
+
+class CandeL3DesignLRFD(CandeBase):
+    pass
