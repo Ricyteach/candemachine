b8ab2bc97ff3e921851f5064e42dd1ece7a79638
Ricky Teachey <ricky@teachey.org>
2019-04-02 03:51:45 +0000

    add _from_dict and subclass method requirement to CandeFormattableMixin

--- candemachine/formats.py
+++ candemachine/formats.py
@@ -4,7 +4,13 @@ from candemachine.exceptions import CandeFormatError
 
 
 class CandeFormattableMixin:
-    _spec_dict = dict(cid="cid_format")
+    _spec_dict = dict(cid="cid_format")  # methods to format to given format key
+    _from_dict = dict(cid="from_cid")  # methods to build instance from given format
+
+    def __init_subclass__(cls, **kwargs):
+        if not all(hasattr(cls, attr) for d in (cls._spec_dict, cls._from_dict) for attr in d.values()):
+            raise CandeFormatError(f'{cls.__qualname__!r} class missing method(s): '
+                                   f'{"".join(attr for d in (cls._spec_dict, cls._from_dict) for attr in d)}')
 
     def __format__(self, format_spec):
         try:
