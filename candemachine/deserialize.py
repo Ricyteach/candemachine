220199606dfff331f4590739efdd330c1738154d
Ricky Teachey <ricky@teachey.org>
2019-04-02 03:57:26 +0000

    deserialize method not worked out yet

--- candemachine/deserialize.py
+++ candemachine/deserialize.py
@@ -2,11 +2,14 @@ from candemachine.exceptions import CandeDeserializationError
 
 
 # TODO: implement this thing
-def deserialize(cls, lines, input="cid"):
-    if input != "cid":
-        raise CandeDeserializationError(f"Input type {input!r} not supported.")
+def deserialize(cls_or_obj, lines, input="cid", parent=None):
     ilines = iter(lines)
-    obj = cls.from_cid(next(ilines))
-    for child in obj.ichildren():
-        child.deserialize(ilines)
-    return obj
+    try:
+        deserializer = cls_or_obj._from_dict[input]
+    except KeyError as e:
+        raise CandeDeserializationError(f"Input type {input!r} not supported.") from e
+    except AttributeError as e:
+        obj = deserializer(next(ilines))
+        for child in obj:
+            child.deserialize(ilines)
+        return obj
